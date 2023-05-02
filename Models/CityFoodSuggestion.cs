9c55cc340d8e68691ae75ec5b10ce0f885c32f32
Adaeze Greathouse <adaezeajenu@Adaezes-MacBook-Pro.local>
2023-05-02 12:07:15 -0500

    added button on homepage and view of form

---
+++ Models/CityFoodSuggestion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace CityFoods.Models
+{
+	public class CityFoodSuggestion
+	{
+		public string City { get; set; }
+		public string NameOfUniqueFood { get; set; }
+		public string RestaurantName { get; set; }
+		public string ImgUrl { get; set; }
+
+		public int Id { get; set; }
+
+
+		public CityFoodSuggestion(string city, string nameOfUniqueFood, string restaurantName, string imgUrl)
+		{
+			City = city;
+			NameOfUniqueFood = nameOfUniqueFood;
+			RestaurantName = restaurantName;
+			ImgUrl = imgUrl;
+		}
+
+		public CityFoodSuggestion() { }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CityFoodSuggestion suggestion &&
+                   Id == suggestion.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
